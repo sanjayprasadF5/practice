87a7be41fc1f66d4a6b45638bc065da2af7d6933
sanjayprasadF5 <sanjayprasad2321998@gmail.com>
2022-11-17 16:55:22 +0530

    strated with brackets

--- DataStructure,Modern,ESC/DestrcutingArray/scripts.js
+++ DataStructure,Modern,ESC/DestrcutingArray/scripts.js
@@ -6,3 +6,13 @@ const restaurant = {
 	starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
 	mainMenu: ["Pizza", "Pasta", "Risotto"],
 }
+
+const arr = [2, 3, 4]
+const a = arr[0]
+const b = arr[1]
+const c = arr[2]
+
+const [x, y, z] = arr
+console.log(x, y, z)
+console.log(a, b, c)
+console.log(arr)
